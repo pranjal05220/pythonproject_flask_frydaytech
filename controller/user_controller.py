30bc362b5444204e56a7f40f021198543a003043
pranjal <srivastavapranjal052@gmail.com>
2023-04-20 22:30:37 +0530

    CHANGES

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,5 +1,6 @@
 from app import app
 from model.user_model import user_model
+from flask import request
 
 obj = user_model()
 
@@ -7,3 +8,12 @@ obj = user_model()
 @app.route("/user/getall")
 def user_getall_controller():
     return obj.user_getall_model()
+
+
+@app.route("/user/addone", methods=["POST", "GET"])
+def user_addone_controller():
+    return obj.user_addone_model(request.form)
+
+@app.route("/user/update", methods=["GET","PUT","POST"])
+def user_update_controller():
+    return obj.user_addone_model(request.form)
